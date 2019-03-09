a0b97844dd71d21a2de4bb18e37c5d6612c7f92b
Ciaran O'Reilly <ciarano@stanford.edu>
2019-03-08 17:31:54 -0800

    Force changes to code.

--- src/main/java/com/github/ctjoreilly/test/Main.java
+++ src/main/java/com/github/ctjoreilly/test/Main.java
@@ -3,6 +3,7 @@ package com.github.ctjoreilly.test;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("===");
     System.out.println("main: "+Main.class.getCanonicalName());
   }
 }
